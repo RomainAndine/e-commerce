4ee626b4056e4386f80d2c12f91dc283f6084449
unknown <romainandine@gmail.com>
2024-01-19 11:15:29 +0100

    Create entity

---
+++ src/Entity/Produit.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProduitRepository::class)]
+class Produit
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $designation = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prixUnit = null;
+
+    #[ORM\Column]
+    private ?int $qteStock = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDesignation(): ?string
+    {
+        return $this->designation;
+    }
+
+    public function setDesignation(?string $designation): static
+    {
+        $this->designation = $designation;
+
+        return $this;
+    }
+
+    public function getPrixUnit(): ?string
+    {
+        return $this->prixUnit;
+    }
+
+    public function setPrixUnit(string $prixUnit): static
+    {
+        $this->prixUnit = $prixUnit;
+
+        return $this;
+    }
+
+    public function getQteStock(): ?int
+    {
+        return $this->qteStock;
+    }
+
+    public function setQteStock(int $qteStock): static
+    {
+        $this->qteStock = $qteStock;
+
+        return $this;
+    }
+}
